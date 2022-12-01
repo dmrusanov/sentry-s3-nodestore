00ae2db4fcffb4c7dcaecb3f2c8d20bbb78b55f6
ilyakuchaev <92860917+ilyakuchaev@users.noreply.github.com>
2022-12-01 14:50:00 +0300

    Update backend.py

--- sentry_s3_nodestore/backend.py
+++ sentry_s3_nodestore/backend.py
@@ -33,7 +33,7 @@ class S3NodeStorage(NodeStorage):
         self.max_retries = max_retries
         self.bucket_name = bucket_name
         config = Config(
-            connect_timeout=5, read_timeout=5
+            connect_timeout=10, read_timeout=10
         )
         self.client = boto3.client('s3', config=config, endpoint_url=endpoint, aws_access_key_id = aws_access_key_id, aws_secret_access_key = aws_secret_access_key)
 
